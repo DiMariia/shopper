72e2b3f77c8b3c82ebb1cbfff5574adf7a18af5e
Demoniolik <mdidenko27@gmail.com>
2022-09-08 20:20:08 +0300

    add header and 3 section to home page

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $(".products__slider").slick({
+        centerPadding: "40px",
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: true,
+        prevArrow:
+            '<button type="button" class="slick-prev"><img  src="src/img/left-arrow.png" alt="slide"></button>',
+        nextArrow:
+            '<button type="button" class="slick-next"><img  src="src/img/right-arrow.png" alt="slide"></button>',
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    dots: true,
+                },
+            },
+        ],
+    });
+});
