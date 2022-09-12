802f89b987f39cc93edef3f9aacbc7e044c26991
Demoniolik <mdidenko27@gmail.com>
2022-09-12 14:42:16 +0300

    add section promo, subscribtion and footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,15 @@
+// Fixed header on scroll
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 60) {
+        $('.header').addClass('fixed');
+    } else {
+        $('.header').removeClass('fixed');
+    }
+});
+
+
+
 $(document).ready(function () {
     $(".products__slider").slick({
         centerPadding: "40px",
@@ -40,3 +52,37 @@ $(document).ready(function () {
         ],
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const deadline = '2022-09-20 23:59:59';
+    let timerId = null;
+    function declensionNum(num, words) {
+      return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+    }
+    function countdownTimer() {
+      const diff = deadline - new Date();
+      if (diff <= 0) {
+        clearInterval(timerId);
+      }
+    const total = Date.parse(deadline) - Date.parse(new Date());
+    const seconds = Math.floor( (total/1000) % 60 );
+    const minutes = Math.floor( (total/1000/60) % 60 );
+    const hours = Math.floor( (total/(1000*60*60)) % 24 );
+    const days = Math.floor( total/(1000*60*60*24) );
+    $days.textContent = days < 10 ? '0' + days : days;
+    $hours.textContent = hours < 10 ? '0' + hours : hours;
+    $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+    $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+    $days.dataset.title = declensionNum(days, ['день', 'дня', 'дней']);
+    $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
+    $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+    $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+        }
+    const $days = document.querySelector('.timer__days');
+    const $hours = document.querySelector('.timer__hours');
+    const $minutes = document.querySelector('.timer__minutes');
+    const $seconds = document.querySelector('.timer__seconds');
+    countdownTimer();
+    timerId = setInterval(countdownTimer, 1000);
+  });
